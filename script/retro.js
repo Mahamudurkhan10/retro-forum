4717d0108f760cbf74fe1fd5adabfa5d4a59d529
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-03-03 01:15:39 +0600

    code sending

---
+++ script/retro.js
@@ -0,0 +1,7 @@
+const kidsCard = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+    const data = await res.json()
+    console.log(data.posts)
+}
+
+kidsCard()
