1bf7fba9ff3bcf254cadb6716d1ceb380f4c4311
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-03-03 22:27:51 +0600

    almost finish

--- script/retro.js
+++ script/retro.js
@@ -1,7 +1,123 @@
-const kidsCard = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+const kidsCard = async (searchText) => {
+    
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`)
     const data = await res.json()
-    console.log(data.posts)
+    const post = data.posts
+    showCard(post)
+    lastestPost()
+    
+}
+const showCard = post => {
+       
+    const cardDiv = document.getElementById('show-card')
+    cardDiv.textContent =''
+    post.forEach(posts => {
+        
+        // console.log( posts)
+
+
+        const div = document.createElement('div')
+        div.innerHTML = `
+    <div class="flex gap-8 font-mulish rounded-3xl bg-[#F3F3F5] p-5">
+    <div class="rounded-full btn  bg-slate-400 "><i class="fa-regular fa-flag"></i></div>
+    <div>
+         <div class="flex gap-6 mb-3">
+             <h1 class="text-lg text-[#12132DCC] font-medium"># ${posts.category} </h1>
+             <h1 class="text-lg text-[#12132DCC] font-medium">Author : <span> ${posts.author.name} </span>  </h1>
+         </div>
+         <div>
+             <h1 class="font-bold text-2xl text-[#12132D] mb-2"> ${posts.title} </h1>
+             <p class="font-normal text-lg"> ${posts.description} </p>
+         </div>
+         <div class="divider"></div>
+         <div class="flex justify-between gap-5">
+             <span><i class="fa-regular fa-message"></i> ${posts.comment_count} </span>
+             <span><i class="fa-regular fa-eye"></i> ${posts.view_count} </span>
+             <span><i class="fa-regular fa-clock"></i> ${posts.posted_time} </span>
+             <div>
+                 <span onclick="mail('${posts.id}')" ><i class="fa-solid fa-envelope" style="color: #63E6BE;"></i></span>
+             </div>
+         </div>
+
+    </div>
+
+    `
+        cardDiv.appendChild(div)
+       
+        
+    });
+    loadingSpiner(false)
+}
+const Search = () => {
+   loadingSpiner(true)
+    const value = document.getElementById('text-here').value;
+   console.log(value)
+   kidsCard(value)
+   if(value ){
+    kidsCard(value)
+   }
+
+   
+}
+const loadingSpiner = (isLoding)=>{
+    const spiner = document.getElementById('loading-spinner')
+    if(isLoding){
+        spiner.classList.remove('hidden')
+    }
+    else{
+        spiner.classList.add('hidden')
+    }
+}
+const mail = async () =>{ 
+    const title = document.getElementById('title-container')
+    const div = document.createElement('div')
+    div.innerHTML =`
+    <h1 class="text-lg font-semibold"> </h1> 
+    <div class="font-semibold text-lg"> <span><i class="fa-regular fa-eye"></i> </span></div>
+    `
+    title.appendChild(div)
+}
+const lastestPost = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+    const data = await res.json();
+  
+    data.forEach(data =>{
+
+        const latestPart = document.getElementById('latest-post')
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card  bg-gray-100 shadow-xl">
+        <figure class="px-10 pt-10">
+          <img src=" ${data.cover_image} " alt="" class="rounded-xl" />
+        </figure>
+      
+            
+
+       
+        <div class="card-body  ">
+            <div class="flex gap-3">
+                <span><i class="fa-regular fa-calendar-minus"></i></span>
+                <span class="flex gap-3">${data.author?.posted_date || 'Not Published'} </span>
+            </div>
+          <h2 class="card-title font-mulish font-extrabold text-lg"> ${data.title} </h2>
+          <p class="font-normal text-lg text-[#12132D99]"> ${data.description} </p>
+          <div class="flex gap-3">
+            <img class="size-11" src=" ${data.profile_image} " alt="">
+            <div>
+                <p class="text-xl font-bold"> ${data.author.name} </p>
+                <p> ${data.author?.designation || "Unknown"} </p>
+            </div>
+        </div>
+
+         
+        </div>
+      </div>
+        `
+        latestPart.appendChild(div)
+    })
+
+    
+
 }
 
-kidsCard()
+kidsCard("comedy")
